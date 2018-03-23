36c3727862361457c96f6d6656bbe6b0ae6e72dc
Perry A Branch <perry.branch@galvanize.com>
2018-03-23 13:00:22 -0600

    Clarify assessment error messages and logic

--- assessment/java/com/galvanize/PillarsTest.java
+++ assessment/java/com/galvanize/PillarsTest.java
@@ -1,12 +1,12 @@
 package com.galvanize;
 
-import static java.util.Collections.singletonList;
+import static com.galvanize.util.ReflectionUtils.failFormat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import com.galvanize.util.ClassProxy;
 import com.galvanize.util.InstanceProxy;
-import net.sf.cglib.proxy.*;
+import com.galvanize.util.MethodBuilder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -18,7 +18,6 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.math.BigDecimal;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,7 +35,7 @@ public class PillarsTest {
             Field[] fields = classProxy.getDelegate().getDeclaredFields();
             for (Field field : fields) {
                 if (Modifier.isPublic(field.getModifiers())) {
-                    fail(String.format("Expected %s.%s to be private or protected, but it is public", className, field.getName()));
+                    failFormat("Expected `%s.%s` to be private or protected, but it is public", className, field.getName());
                 }
             }
         }
@@ -54,42 +53,11 @@ public class PillarsTest {
 
             Class<?> superclass = classProxy.getDelegate().getSuperclass();
             if (superclass == Object.class) {
-                fail(String.format("Expected %s to inherit from some other class, but it inherits from Object", className));
+                failFormat("Expected `%s` to inherit from some other class, but it inherits from `Object`", className);
             } else if (!Modifier.isAbstract(superclass.getModifiers())) {
-                fail(String.format("Expected %s to be abstract", superclass.getName()));
+                failFormat("Expected `%s` to be abstract", superclass.getName());
             }
         }
-
-        @Test()
-        @DisplayName("Item classes should inherit from a base class")
-        public void itemClassesShouldInheritFromABaseClass() {
-            ClassProxy Lease = ClassProxy.classNamed("com.galvanize.Lease");
-
-            ClassProxy Item = Lease.getSuperclassProxy();
-
-            String methodName = "totalPrice";
-            BigDecimal result = new BigDecimal("999.99");
-
-            InstanceProxy item = Item.subclass()
-                    .intercept(methodName, result)
-                    .build();
-
-            ClassProxy Order = ClassProxy.classNamed("com.galvanize.Order")
-                    .ensureConstructor()
-                    .ensureMethod(m -> m.named("addItem").withParameters(Item))
-                    .ensureMethod(m -> m.named("getTotal").returns(BigDecimal.class))
-                    .ensureMethod(m -> m.named("getItems"));
-
-            InstanceProxy order = Order.newInstance();
-            order.invoke("addItem", item);
-
-            BigDecimal totalAmount = (BigDecimal) order.invoke("getTotal");
-            List<?> items = (List<?>) order.invoke("getItems");
-
-            assertEquals(new BigDecimal("999.99"), totalAmount);
-            assertEquals(singletonList(item.getDelegate()), items);
-        }
-
     }
 
     @Nested
@@ -98,25 +66,51 @@ public class PillarsTest {
         @Test
         @DisplayName("Order.addItem should be polymorphic")
         public void addShouldBePolymorphic() throws Throwable {
-            ClassProxy Order = ClassProxy.classNamed("com.galvanize.Order");
 
-            Method[] declaredMethods = Order.getDelegate().getDeclaredMethods();
-            Optional<Method> addItemMethod = Arrays.stream(declaredMethods)
+            Method[] declaredMethods = Order.class.getDeclaredMethods();
+            Method addItemMethod = Arrays.stream(declaredMethods)
                     .filter(method -> method.getName().equals("addItem"))
-                    .findFirst();
+                    .findFirst().orElseThrow(() -> fail("Expected `Order` to have a method named `addItem`"));
 
-            addItemMethod.ifPresent(method -> {
-                Class<?>[] parameterTypes = method.getParameterTypes();
-                assertEquals(1, parameterTypes.length, "Expected Order#addItems to take one parameter, but got " + parameterTypes.length);
+            Class<?>[] parameterTypes = addItemMethod.getParameterTypes();
+            assertEquals(1, parameterTypes.length, "Expected `Order.addItem` to take one parameter, but got " + parameterTypes.length);
 
-                if (parameterTypes[0] == Object.class) {
-                    fail("Expected the parameter type of Order#addItems to be something other than Object");
-                }
-            });
+            Class<?> itemClass = parameterTypes[0];
+            if (itemClass == Object.class) {
+                fail("Expected the parameter type of `Order.addItem` to be something other than `Object`");
+            }
 
-            addItemMethod.orElseThrow(() -> fail("Expected Order to have a method named `addItem`"));
+            ClassProxy _Item = ClassProxy.of(itemClass);
+            String methodName = "totalPrice";
+            _Item.ensureMethod(m -> m.named(methodName).returns(BigDecimal.class));
+
+            BigDecimal result = new BigDecimal("999.99");
+            InstanceProxy item = _Item.subclass()
+                    .intercept(methodName, result)
+                    .build();
+
+            ClassProxy _Order = ClassProxy.of(Order.class)
+                    .ensureConstructor()
+                    .ensureMethod(addItemMethod)
+                    .ensureMethod(m -> m.named("getTotal").returns(BigDecimal.class))
+                    .ensureMethod(m -> m.named("getItems"));
+
+            InstanceProxy order = _Order.newInstance();
+            order.invoke("addItem", item);
+
+            BigDecimal totalAmount = (BigDecimal) order.invoke("getTotal");
+            List<?> items = (List<?>) order.invoke("getItems");
+
+            if (items.size() != 1 || !items.get(0).equals(item.getDelegate())) {
+                fail("Expected `Order.addItem` to add the specified item to the list");
+            }
+
+            if (result.compareTo(totalAmount) != 0) {
+                failFormat(
+                        "Expected `Order` to call `%s` for any implementation of the `%s` base class",
+                        methodName, _Item.getDelegate().getSimpleName());
+            }
         }
-
     }
 
 }
